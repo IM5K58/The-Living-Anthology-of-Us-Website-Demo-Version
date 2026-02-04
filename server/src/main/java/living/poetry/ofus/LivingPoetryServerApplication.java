dd2cba6fa510293cea4dc6e92016d718bb5ebfeb
IM5K58 <gitue11@gmail.com>
2026-02-04 21:30:19 +0900

    feat:AuditingEntityListener 추가

--- server/src/main/java/living/poetry/ofus/LivingPoetryServerApplication.java
+++ server/src/main/java/living/poetry/ofus/LivingPoetryServerApplication.java
@@ -2,7 +2,9 @@ package living.poetry.ofus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LivingPoetryServerApplication {
 
